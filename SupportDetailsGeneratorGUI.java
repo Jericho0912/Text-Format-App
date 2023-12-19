f41095d06836984ef7d3eef59756679b7d41289a
Jericho Del Rosario <jerichodelrosario35@gmail.com>
2023-12-19 12:06:01 +0800

    compiled and working

--- SupportDetailsGeneratorGUI.java
+++ SupportDetailsGeneratorGUI.java
@@ -7,6 +7,9 @@ import java.awt.*;
 import java.awt.datatransfer.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -15,7 +18,7 @@ import java.util.Properties;
 public class SupportDetailsGeneratorGUI {
 
     private JFrame frame;
-    private JTextField issueField, acknowledgeField, resolutionField;
+    private JTextField issueField, acknowledgeField, resolutionField, issuedForField, remarks ;
     private JComboBox<String> statusComboBox;
     private JDatePickerImpl raisedDatePicker, closedDatePicker;
     private JSpinner raisedTimeSpinner, closedTimeSpinner;
@@ -33,9 +36,9 @@ public class SupportDetailsGeneratorGUI {
 
     public void initialize() {
         frame = new JFrame();
-        frame.setBounds(100, 100, 500, 400);
+        frame.setBounds(100, 100, 500, 700);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        frame.setTitle("Support Details Generator");
         JPanel panel = new JPanel(new GridBagLayout());
         frame.getContentPane().add(panel, BorderLayout.CENTER);
 
@@ -47,7 +50,8 @@ public class SupportDetailsGeneratorGUI {
         gbc.gridx = 0;
 
         panel.add(new JLabel("Issue/Request:"), gbc);
-        panel.add(new JLabel("Acknowledge by:"), gbc);
+        panel.add(new JLabel("Ticket Issued to:"), gbc);
+        panel.add(new JLabel("Acknowledged by:"), gbc);
         panel.add(new JLabel("Status:"), gbc);
         panel.add(new JLabel("Resolution:"), gbc);
         panel.add(new JLabel("Date Raised:"), gbc);
@@ -62,6 +66,10 @@ public class SupportDetailsGeneratorGUI {
         issueField = new JTextField();
         gbc.gridwidth = 2; // Span across 2 columns
         panel.add(issueField, gbc);
+        
+        issuedForField = new JTextField();
+        gbc.gridwidth = 2; // Span across 2 columns
+        panel.add(issuedForField, gbc);
 
         acknowledgeField = new JTextField();
         panel.add(acknowledgeField, gbc);
@@ -84,20 +92,34 @@ public class SupportDetailsGeneratorGUI {
 
         closedTimeSpinner = createTimeSpinner();
         panel.add(closedTimeSpinner, gbc);
+        
+        // remarks = new JTextField();
+        // panel.add(remarks,gbc);
+        gbc.gridy = 9; // Adjust the gridy value as needed
+        gbc.gridx = 0;
+        JLabel remarksLabel = new JLabel("Remarks:");
+        panel.add(remarksLabel, gbc);       
+        //remarks
+        gbc.gridy = 10; // Adjust the gridy value as needed
+        gbc.gridx = 0;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        JTextField remarks = new JTextField();
+        panel.add(remarks, gbc);
 
         // Text area
-        gbc.gridy = 8;
+        gbc.gridy = 11; // Adjust the gridy value to the next row
         gbc.gridx = 0;
         gbc.gridwidth = GridBagConstraints.REMAINDER; // Span across both columns
         gbc.fill = GridBagConstraints.BOTH;
 
         resultTextArea = new JTextArea();
         panel.add(new JScrollPane(resultTextArea), gbc);
+        remarks.setPreferredSize(new Dimension(200, 60));
 
         // Copy-Paste button
-        gbc.gridy = 9;
-        gbc.gridx = 0;
-        gbc.gridwidth = GridBagConstraints.REMAINDER; // Span across both columns
+        gbc.gridy = 12; // Adjusted gridy value
+        gbc.gridx = 1;
+        gbc.gridwidth = 1; // Span only one column
         gbc.fill = GridBagConstraints.NONE;
         gbc.anchor = GridBagConstraints.CENTER;
 
@@ -105,8 +127,10 @@ public class SupportDetailsGeneratorGUI {
         panel.add(copyButton, gbc);
 
         JButton submitButton = new JButton("Submit");
-        gbc.gridy = 10;
+        gbc.gridx = 1;
+        gbc.gridy = 16; 
         panel.add(submitButton, gbc);
+
 
         submitButton.addActionListener(new ActionListener() {
             @Override
@@ -115,19 +139,29 @@ public class SupportDetailsGeneratorGUI {
                 Date raisedTime = (Date) raisedTimeSpinner.getValue();
                 Date closedDate = (Date) closedDatePicker.getModel().getValue();
                 Date closedTime = (Date) closedTimeSpinner.getValue();
-
-                String result = "Support Details:\n" +
+        
+                // Check if raisedDate and closedDate are not null before formatting
+                String result = 
+                        "Good Day Ms/Mr :" + issuedForField.getText() + "\n" +
+                        "\n" +
+                        "Support Details:\n" +
                         "Issue/Request: " + issueField.getText() + "\n" +
-                        "Acknowledge by: " + acknowledgeField.getText() + "\n" +
+                        "Ticket Request from:" +issuedForField.getText() + "\n" +
+                        "Acknowledged by: " + acknowledgeField.getText() + "\n" +
                         "Status: " + statusComboBox.getSelectedItem() + "\n" +
                         "Resolution: " + resolutionField.getText() + "\n" +
-                        "Date Raised: " + formatDate(raisedDate) + "\n" +
+                        "Date Raised: " + (raisedDate != null ? formatDate(raisedDate) : "") + "\n" +
                         "Time Raised: " + formatTime(raisedTime) + "\n" +
-                        "Date Closed: " + formatDate(closedDate) + "\n" +
-                        "Time Closed: " + formatTime(closedTime) + "\n";
+                        "Date Closed: " + (closedDate != null ? formatDate(closedDate) : "") + "\n" +
+                        "Time Closed: " + formatTime(closedTime) + "\n" +
+                        "Remarks: " + remarks.getText() + "\n" +
+                        "\n" +
+                        "Thank you for your patience :)";
+
                 resultTextArea.setText(result);
             }
         });
+        
 
         copyButton.addActionListener(new ActionListener() {
             @Override
